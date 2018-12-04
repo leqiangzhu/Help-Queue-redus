b99d62b4c0c8498829fa833bdb118e2ca5f306dd
Connor McCarthy and David Zhu <connor@>
2018-12-04 11:39:40 -0800

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import PropTypes from 'prop-types';
+import Moment from 'moment';
+// import Admin from './Admin';
+import { v4 } from 'uuid';
+import { connect } from 'react-redux';
+import { Switch, Route, withRouter } from 'react-router-dom';
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      masterTicketList: {},
+      selectedTicket: null
+    };
+    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+      60000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime() {
+    // var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
+    // Object.keys(newMasterTicketList).forEach(ticketId => {
+    //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
+    // });
+    // this.setState({ masterTicketList: newMasterTicketList });
+  }
+
+  handleChangingSelectedTicket(ticketId) {
+    this.setState({ selectedTicket: ticketId });
+  }
+
+  render() {
+    console.log(this.state.masterTicketList);
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' render={() => <TicketList ticketList={this.props.masterTicketList} />} />
+          <Route path='/newticket' render={() => <NewTicketControl />} />
+          <Route path='/admin' render={(props) => <Admin ticketList={this.props.masterTicketList} currentRouterPath={props.location.pathname}
+            onTicketSelection={this.handleChangingSelectedTicket}
+            selectedTicket={this.props.selectedTicket} />} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    masterTicketList: state
+  }
+}
+
+App.propTypes = {
+  masterTicketList: PropTypes.object
+};
+
+export default withRouter(connect(mapStateToProps)(App));
